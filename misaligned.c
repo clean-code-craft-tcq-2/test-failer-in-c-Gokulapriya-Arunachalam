f86ea38857a6dd25f12432a29de3703771b9a44b
Gokulapriya-Arunachalam <91065590+Gokulapriya-Arunachalam@users.noreply.github.com>
2022-02-03 11:14:34 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
+#include "Color_Pair_Basic_Info.h"
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+unsigned int colorCode_order = 0;
 
-typedef struct {
-    const char* majorColor;
-    const char* minorColor;
-} ColorPair;
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair color_Pair;
     color_Pair.majorColor = MajorColorNames[(pairNumber / numberOfMinorColors)];
@@ -37,23 +24,25 @@ void testNumberToPair(int pairNumber,
     assert(colorPair.minorColor == expectedMinor);
 }
 
-int printColorMap() {
+void printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            testNumberToPair(i * 5 + j,  majorColor[i], minorColor[i]);
+            printf("%d | %s | %s\n", colorCode_order , majorColor[i], minorColor[j]);           
+            colorCode_order++;
         }
     }
-    return i * j;
+   
     
 }
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    printColorMap();
+    testNumberToPair(0,  "White", "Blue");
+    testNumberToPair(1,  "White", "Orange");
+    
     printf("All is well (maybe!)\n");
     return 0;
 }
